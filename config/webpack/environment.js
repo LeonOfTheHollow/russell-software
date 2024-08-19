35a7c5825d1465491dc9657b08d8ccf422d5f3a5
LeonOfTheHollow <russell.l.bates@gmail.com>
2024-08-19 17:41:04 -0500

    tweaks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,11 @@
 const { environment } = require('@rails/webpacker')
 const sassLoader = require('./loaders/sass.js')
 
-environment.loaders.prepend('sass', sassLoader)
+// Check if environment.loaders exists before trying to use it
+if (environment.loaders) {
+  environment.loaders.prepend('sass', sassLoader)
+} else {
+  console.warn('environment.loaders is undefined. Skipping sass loader configuration.')
+}
 
 module.exports = environment
